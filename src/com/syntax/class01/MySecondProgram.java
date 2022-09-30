96b5530b1eefa509617f6adca02904dc12d7f861
elenanij <elenanij@gmail.com>
2022-09-29 21:20:40 -0700

    first java commit

---
+++ src/com/syntax/class01/MySecondProgram.java
@@ -0,0 +1,16 @@
+package com.syntax.class01;
+
+public class MySecondProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//this is a single line comment 
+		/*this is 
+		 * a multiline
+		 * comment
+		 */
+		
+		System.out.println("This is my second Program");
+	}
+
+}
---
+++ src/com/syntax/class01/PrimitiveDataTypes.java
@@ -0,0 +1,39 @@
+package com.syntax.class01;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//integers
+		
+		byte box1 = 127;
+		
+		short box2 = 100;
+		
+		int box3 = 100000; //popular
+		
+		long box4 = 1000000; //popular
+		
+		//decimals
+		
+		float variable1 = 1.99f;
+		
+		double variable2 = 99.99; //popular
+		
+		//single characters
+		
+		char container = 'A';
+		
+		char dollar = '$';
+		
+		//boolean 
+		
+		boolean hot = true;
+		
+		boolean cold = false;
+		
+		System.out.print(cold);
+	}
+
+}
